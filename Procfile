ca3b3f0440e7ebf0db658afdc48d869bd8112ab6
ghostpete <ghostcoderme@gmail.com>
2026-05-31 21:38:44 +0100

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tradeverde.wsgi:application --bind 0.0.0.0:$PORT
---
+++ tradeverde/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tradeverde project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tradeverde.settings')
+
+application = get_asgi_application()
